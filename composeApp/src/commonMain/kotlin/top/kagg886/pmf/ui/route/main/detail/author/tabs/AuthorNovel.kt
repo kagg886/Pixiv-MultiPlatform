7ded44c058e5f042c95a1b88063ed75d7c8d1727
kagg886 <iveour@163.com>
2024-10-13 22:22:52 +0800

    feat: ✨ 支持查看作者小说

--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/route/main/detail/author/tabs/AuthorNovel.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/route/main/detail/author/tabs/AuthorNovel.kt
@@ -1,10 +1,43 @@
 package top.kagg886.pmf.ui.route.main.detail.author.tabs
 
 import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.model.rememberScreenModel
+import top.kagg886.pixko.module.novel.Novel
+import top.kagg886.pixko.module.novel.NovelResult
+import top.kagg886.pixko.module.novel.getRecommendNovel
+import top.kagg886.pixko.module.novel.getRecommendNovelNext
 import top.kagg886.pixko.module.user.UserInfo
+import top.kagg886.pixko.module.user.getUserNovel
+import top.kagg886.pmf.backend.pixiv.InfinityRepository
 import top.kagg886.pmf.ui.component.ErrorPage
+import top.kagg886.pmf.ui.route.main.detail.author.AuthorScreen
+import top.kagg886.pmf.ui.util.IllustFetchScreen
+import top.kagg886.pmf.ui.util.NovelFetchScreen
+import top.kagg886.pmf.ui.util.NovelFetchViewModel
+import kotlin.coroutines.CoroutineContext
 
 @Composable
-fun AuthorNovel(user: UserInfo) {
-    ErrorPage(text = "等待更新哦~") {}
+fun AuthorScreen.AuthorNovel(user: UserInfo) {
+    val model = rememberScreenModel(tag = "user_novel_${user.user.id}") {
+        AuthorNovelViewModel(user.user.id.toLong())
+    }
+    NovelFetchScreen(model)
+}
+
+class AuthorNovelViewModel(val id: Long) : NovelFetchViewModel() {
+    override fun initInfinityRepository(coroutineContext: CoroutineContext): InfinityRepository<Novel> {
+        return object : InfinityRepository<Novel>(coroutineContext) {
+            var page = 1
+            override suspend fun onFetchList(): List<Novel>? {
+                val result = kotlin.runCatching {
+                    client.getUserNovel(id, page)
+                }
+                if (result.isFailure) {
+                    return null
+                }
+                return result.getOrThrow().ifEmpty { null }
+            }
+        }
+    }
+
 }
