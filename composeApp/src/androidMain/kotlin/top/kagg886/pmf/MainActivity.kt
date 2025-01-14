e5ba34299eb0481fd43734314d2bd1933ec0ee88
kagg886 <iveour@163.com>
2025-01-14 08:54:23 +0800

    fix: 🐛 修复沉浸式导航栏

--- composeApp/src/androidMain/kotlin/top/kagg886/pmf/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/top/kagg886/pmf/MainActivity.kt
@@ -2,12 +2,15 @@ package top.kagg886.pmf
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import io.github.vinceglb.filekit.core.FileKit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         FileKit.init(this)
         setContent {
             App()
