be9898c59ec290970d00baa0f466423730717a7a
kagg886 <iveour@163.com>
2025-02-07 16:32:37 +0800

    build: ⬆️ 升级kotlin版本

--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/backend/database/dao/search-history.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/backend/database/dao/search-history.kt
@@ -5,8 +5,6 @@ import kotlinx.coroutines.flow.Flow
 import top.kagg886.pixko.module.search.SearchSort
 import top.kagg886.pixko.module.search.SearchTarget
 import top.kagg886.pmf.backend.database.converters.HistoryConverter
-import top.kagg886.pmf.backend.database.converters.NovelConverter
-import top.kagg886.pmf.ui.route.main.search.SearchTab
 
 
 @Dao
@@ -31,14 +29,4 @@ data class SearchHistory(
     val initialTarget: SearchTarget,
     val keyword: List<String> = listOf(),
     val createTime: Long = System.currentTimeMillis(),
-
-
-
-
-
-
-    @Deprecated("v2版本使用数组存储")
-    val initialKeyWords: String = "",
-    @Deprecated("v2版本不使用此属性")
-    val tab: SearchTab = SearchTab.ILLUST,
 )
