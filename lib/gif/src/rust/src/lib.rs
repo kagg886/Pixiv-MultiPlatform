c5cc26239b933e47c89601b4a253b1a898de894c
Dude so hot <djohn@fbi.gov>
2025-04-03 00:21:24 +0800

    Done

--- lib/gif/src/rust/src/lib.rs
+++ lib/gif/src/rust/src/lib.rs
@@ -1,15 +1,15 @@
 #![feature(lazy_get)]
 use anyhow::Result;
-use futures::{executor::block_on, stream, StreamExt};
 use gif::{DisposalMethod, Encoder, Frame, Repeat};
 use serde::Deserialize;
 use std::{
     fs::File,
     ptr::slice_from_raw_parts,
-    sync::{LazyLock, OnceLock},
+    sync::{Arc, LazyLock, OnceLock},
 };
+use tokio::runtime::Runtime;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 #[allow(non_snake_case)]
 struct UgoiraFrame {
     file: String,
@@ -24,15 +24,15 @@ struct GifEncodeRequest {
     dstPath: String,
 }
 
-async fn encode_animated_image(src_buffer: &[u8]) -> Result<()> {
+async fn encode_animated_image(src_buffer: &[u8], rt: &Runtime) -> Result<()> {
     let GifEncodeRequest {
         metadata,
         speed,
         dstPath,
     } = serde_cbor::from_slice(src_buffer)?;
     let dst_file = File::create(dstPath)?;
-    let dimen = &OnceLock::new();
-    let encoder = LazyLock::new(|| {
+    let dimen = Arc::new(OnceLock::new());
+    let mut encoder = LazyLock::new(|| {
         let (w, h) = dimen.get().unwrap();
         Encoder::new(dst_file, *w, *h, &[])
             .and_then(|mut encoder| {
@@ -41,29 +41,33 @@ async fn encode_animated_image(src_buffer: &[u8]) -> Result<()> {
             })
             .unwrap()
     });
-    let stream = stream::iter(metadata).filter_map(|frame_info| async move {
-        let file = frame_info.file;
-        println!("Start decoding image {file}");
-        let image = image::open(file.clone()).ok()?;
-        let (width, height) = (image.width() as u16, image.height() as u16);
-        dimen.get_or_init(|| (width, height));
-        let mut frame = Frame::from_rgba_speed(width, height, &mut image.to_rgba8(), speed);
-        println!("End decoding image {file}");
-        frame.delay = (frame_info.delay / 10) as u16;
-        frame.dispose = DisposalMethod::Background;
-        Some(frame)
-    });
-    stream
-        .fold(encoder, |mut encoder, frame| async move {
+    let f = |frame_info: UgoiraFrame| {
+        let dimen = dimen.clone();
+        async move {
+            let file = &frame_info.file;
+            let image = image::open(file.clone()).ok()?;
+            let (width, height) = (image.width() as u16, image.height() as u16);
+            dimen.get_or_init(|| (width, height));
+            let mut frame = Frame::from_rgba_speed(width, height, &mut image.to_rgba8(), speed);
+            frame.delay = (frame_info.delay / 10) as u16;
+            frame.dispose = DisposalMethod::Background;
+            Some(frame)
+        }
+    };
+    for batch in metadata.chunks(8) {
+        let stream: Vec<_> = batch.iter().map(|u| rt.spawn(f(u.clone()))).collect();
+        for hnd in stream {
+            let frame = hnd.await.unwrap().unwrap();
             LazyLock::force_mut(&mut encoder).write_frame(&frame).ok();
-            encoder
-        })
-        .await;
+        }
+    }
     Ok(())
 }
 
 #[no_mangle]
 pub extern "C" fn encode_animated_image_unsafe(ptr: *const u8, len: i32) {
     let slice = unsafe { &*slice_from_raw_parts(ptr, len as usize) };
-    block_on(encode_animated_image(slice)).unwrap();
+    static RT: OnceLock<Runtime> = OnceLock::new();
+    let rt = RT.get_or_init(|| Runtime::new().unwrap());
+    rt.block_on(encode_animated_image(slice, rt)).unwrap();
 }
