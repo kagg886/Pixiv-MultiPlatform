349fe8e2a8901a8316d16f803851fe1497b8c844
Dude so hot <djohn@fbi.gov>
2025-04-03 16:15:30 +0800

    Update test

--- lib/gif/src/linuxX64Test/kotlin/GifEncoder.native.kt
+++ lib/gif/src/linuxX64Test/kotlin/GifEncoder.native.kt
@@ -1,4 +1,5 @@
 import kotlin.test.Test
+import moe.tarsin.gif.Frame
 import moe.tarsin.gif.GifEncodeRequest
 import moe.tarsin.gif.encodeGifPlatform
 
@@ -6,7 +7,11 @@ class NativeTest {
 
     @Test
     fun testNative() {
-        val req = GifEncodeRequest(emptyList(), 0, "")
+        val data = listOf(
+            Frame("/home/tarsin/Pictures/屏幕截图/屏幕截图_20250327_121207.png", 100),
+            Frame("/home/tarsin/Pictures/屏幕截图/屏幕截图_20250330_222527.png", 100),
+        )
+        val req = GifEncodeRequest(data, 15, "/home/tarsin/Pictures/test.gif")
         encodeGifPlatform(req)
     }
 }
