85823849099d1b973e9b1acb0058d64be2fd0ccd
Dude so hot <djohn@fbi.gov>
2025-04-05 00:57:38 +0800

    Refine exportLib

--- lib/gif/src/jvmMain/kotlin/moe/tarsin/gif/GifEncoder.jvm.kt
+++ lib/gif/src/jvmMain/kotlin/moe/tarsin/gif/GifEncoder.jvm.kt
@@ -1,13 +1,14 @@
 package moe.tarsin.gif
 
 import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.encodeToByteArray
+import okio.BufferedSource
 import okio.Path
 import okio.Path.Companion.toPath
-import okio.Source
 import okio.buffer
 import okio.hashingSource
 import okio.source
@@ -29,6 +30,8 @@ internal actual fun encodeGifPlatform(request: GifEncodeRequest) {
     }
 }
 
+internal inline fun <R> useRes(name: String, f: BufferedSource.() -> R) = GIFEncoderBuilderScope::class.java.getResourceAsStream(name)!!.source().buffer().use(f)
+
 @Suppress("UnsafeDynamicallyLoadedCode")
 internal actual fun loadNativeGifEncoder() {
     val name = when (jvmTarget) {
@@ -37,33 +40,20 @@ internal actual fun loadNativeGifEncoder() {
         JvmTarget.MACOS -> "libgif_rust.dylib"
     }
 
-    val libPath = System.getProperty("user.home").toPath()
-        .resolve(".config")
-        .resolve("pmf")
-        .resolve("lib")
-        .resolve(name)
-
+    val home = System.getProperty("user.home").toPath()
+    val libPath = home / ".config" / "pmf" / "lib" / name
     if (!libPath.exists()) {
-        val stream = GIFEncoderBuilderScope::class.java.getResourceAsStream("/$name")!!.source()
-        exportLibToPath(stream, libPath)
+        useRes("/$name") { exportLibToPath(libPath) }
     } else {
-        val newHash = GIFEncoderBuilderScope::class.java.getResourceAsStream("/gif-build.hash")!!.readBytes().decodeToString()
-        val oldHash = (libPath.source() as Source).use { it.hashingSource(MessageDigest.getInstance("MD5")).hash.hex().lowercase() }
-        if (newHash != oldHash) {
-            val stream = GIFEncoderBuilderScope::class.java.getResourceAsStream("/$name")!!.source()
-            exportLibToPath(stream, libPath)
-        }
+        val newHash = useRes("/gif-build.hash") { readString(StandardCharsets.UTF_8) }
+        val oldHash = libPath.source().use { it.hashingSource(MessageDigest.getInstance("MD5")).hash.hex().lowercase() }
+        if (newHash != oldHash) useRes("/$name") { exportLibToPath(libPath) }
     }
     System.load(libPath.absolutePath().toString())
 }
 
-private fun exportLibToPath(stream: Source, libPath: Path) {
+private fun BufferedSource.exportLibToPath(libPath: Path) {
     libPath.parentFile()!!.mkdirs()
     libPath.createNewFile()
-    stream.use { i ->
-        libPath.sink().buffer().use { o ->
-            o.write(i.buffer().readByteArray())
-            o.flush()
-        }
-    }
+    libPath.sink().use(::readAll)
 }
