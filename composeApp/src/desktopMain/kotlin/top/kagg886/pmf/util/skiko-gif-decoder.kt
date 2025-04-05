d91acdb01b89fe611523bc872b63bcc3e8eed32e
Dude so hot <djohn@fbi.gov>
2025-04-05 18:48:32 +0800

    Comments about hack

--- composeApp/src/desktopMain/kotlin/top/kagg886/pmf/util/skiko-gif-decoder.kt
+++ composeApp/src/desktopMain/kotlin/top/kagg886/pmf/util/skiko-gif-decoder.kt
@@ -65,6 +65,29 @@ class AnimatedSkiaImage(
         frameDurationsMs.sum()
     }
 
+    // Well, we are only allowed to draw [SkImage] **not** [SKBitmap] on [SkCanvas]
+    // But we are only allowed to decode a [SkCodec] frame to [SKBitmap]
+    // If we reuse [SKBitmap]'s memory in decode process, [SKBitmap] is mutable
+    // Convert it to [SkImage] using [SkiaImage.makeFromBitmap] will copy underlying pixels
+    // That's waste, and any update(i.e. reuse for decode) on bitmap will not reflect on [SkImage]
+    // If we use [SKBitmap] as immutable, we must allocate many many instances to decode.
+    //
+    // Let's save out memory, we pretend [SKBitmap] is immutable by calling [setImmutable()], so
+    // [SkiaImage.makeFromBitmap] will let [SkImage] share [SKBitmap]'s pixels memory
+    // Then we force mutate [SKBitmap](i.e. reuse to decode), well, redraw the same [SkImage]
+    // instance doesn't make a difference? Call [notifyPixelsChanged()] after decode(update underlying memory).
+
+    // 我们只能在 [SkCanvas] 上绘制 [SkImage] **而不是** [SKBitmap]
+    // 但我们同时只能把 [SkCodec] 解码到 [SKBitmap]
+    // 如果我们在解码过程中重用 [SKBitmap] 的内存，那我们需要要求 [SKBitmap] 是可变的
+    // 使用 [SkiaImage.makeFromBitmap] 将可变的位图转换为 [SkImage] 会复制底层像素
+    // 太浪费了! 并且位图内存上的任何更新（在解码过程中重用它）都不会更新在 [SkImage] 上
+    // 如果我们把 [SKBitmap] 用作不可变的，我们必须分配很多实例来解码。
+    //
+    // 省省吧，我们通过调用 [setImmutable()] 假装 [SKBitmap] 是不可变的，所以
+    // [SkiaImage.makeFromBitmap] 会让 [SkImage] 共享 [SKBitmap] 的像素内存
+    // 然后我们强制更新 [SKBitmap]（即重新用于解码），但是重新使用相同的 [SkImage] 实例进行绘制
+    // 没有区别？我们忘了更新像素内存后调用 [notifyPixelsChanged()]。
     private val bitmapA = Bitmap().apply {
         allocPixels(codec.imageInfo)
         setImmutable()
