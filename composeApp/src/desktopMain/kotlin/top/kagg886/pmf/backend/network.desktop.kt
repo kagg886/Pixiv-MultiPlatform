eb729efe3c7d1db370165b7322628f04581510f6
Dude so hot <djohn@fbi.gov>
2025-04-20 19:37:05 +0800

    Use java HttpClient when no need to bypass sni

--- composeApp/src/desktopMain/kotlin/top/kagg886/pmf/backend/network.desktop.kt
+++ composeApp/src/desktopMain/kotlin/top/kagg886/pmf/backend/network.desktop.kt
@@ -1,39 +1,49 @@
 package top.kagg886.pmf.backend
 
-import io.ktor.client.*
-import io.ktor.client.engine.*
-import io.ktor.client.engine.okhttp.*
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.java.Java
+import io.ktor.client.engine.java.JavaHttpConfig
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.engine.okhttp.OkHttpConfig
 import java.net.InetSocketAddress
 import java.net.Proxy
+import java.net.http.HttpClient
 import top.kagg886.pmf.util.logger
 
-private val InternalType: HttpClientConfig<OkHttpConfig>.() -> Unit = {
+private val InternalType: HttpClientConfig<HttpClientEngineConfig>.() -> Unit = {
     engine {
-        config {
-            followRedirects(true)
-            when (val config = AppConfig.bypassSettings) {
-                AppConfig.BypassSetting.None -> Unit
-
-                is AppConfig.BypassSetting.SNIReplace -> {
-                    bypassSNIOnDesktop(
-                        queryUrl = config.url,
-                        unsafeSSL = config.nonStrictSSL,
-                        fallback = config.fallback,
-                        dohTimeout = config.dohTimeout,
-                    )
-                }
-
-                is AppConfig.BypassSetting.Proxy -> {
-                    this.proxy(
-                        Proxy(Proxy.Type.valueOf(config.type.toString()), InetSocketAddress.createUnresolved(config.host, config.port)),
-                    )
-                }
+        when (val config = AppConfig.bypassSettings) {
+            AppConfig.BypassSetting.None -> (this as JavaHttpConfig).apply {
+                protocolVersion = HttpClient.Version.HTTP_2
+                config { followRedirects(HttpClient.Redirect.ALWAYS) }
             }
-            logger.i("BypassType: " + AppConfig.bypassSettings::class.simpleName)
+
+            is AppConfig.BypassSetting.SNIReplace -> (this as OkHttpConfig).config {
+                followRedirects(true)
+                bypassSNIOnDesktop(
+                    queryUrl = config.url,
+                    unsafeSSL = config.nonStrictSSL,
+                    fallback = config.fallback,
+                    dohTimeout = config.dohTimeout,
+                )
+            }
+
+            is AppConfig.BypassSetting.Proxy -> (this as JavaHttpConfig).apply {
+                protocolVersion = HttpClient.Version.HTTP_2
+                proxy = Proxy(Proxy.Type.valueOf(config.type.toString()), InetSocketAddress.createUnresolved(config.host, config.port))
+                config { followRedirects(HttpClient.Redirect.ALWAYS) }
+            }
         }
+        logger.i("BypassType: " + AppConfig.bypassSettings::class.simpleName)
     }
 }
 
-actual val PlatformEngine: HttpClientEngineFactory<*> = OkHttp
+actual val PlatformEngine: HttpClientEngineFactory<*> = when (AppConfig.bypassSettings) {
+    AppConfig.BypassSetting.None, is AppConfig.BypassSetting.Proxy -> Java
+    is AppConfig.BypassSetting.SNIReplace -> OkHttp
+}
 
+@Suppress("UNCHECKED_CAST")
 actual val PlatformConfig0: HttpClientConfig<*>.() -> Unit = InternalType as HttpClientConfig<*>.() -> Unit
