9cce3b6285884a6a6b009dd173fc284d0e3b9a3a
Dude so hot <djohn@fbi.gov>
2025-05-10 21:53:24 +0800

    Kill infirepo

--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/route/main/detail/novel/NovelCommentViewModel.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/route/main/detail/novel/NovelCommentViewModel.kt
@@ -1,23 +1,17 @@
 package top.kagg886.pmf.ui.route.main.detail.novel
 
-import top.kagg886.pixko.module.illust.Comment
 import top.kagg886.pixko.module.novel.getNovelComment
 import top.kagg886.pixko.module.novel.getNovelCommentReply
 import top.kagg886.pixko.module.novel.sendNovelComment
-import top.kagg886.pmf.backend.pixiv.InfinityRepository
 import top.kagg886.pmf.backend.pixiv.PixivConfig
 import top.kagg886.pmf.ui.util.CommentViewModel
+import top.kagg886.pmf.ui.util.flowOf
+import top.kagg886.pmf.ui.util.page
 
 class NovelCommentViewModel(id: Long) : CommentViewModel(id) {
     val client = PixivConfig.newAccountFromConfig()
-    override suspend fun fetchComments(id: Long, page: Int): List<Comment> = client.getNovelComment(id, page)
-
-    override suspend fun fetchCommentReply(commentId: Long): InfinityRepository<Comment> = object : InfinityRepository<Comment>() {
-        var page = 1
-        override suspend fun onFetchList(): List<Comment> = client.getNovelCommentReply(commentId, page).apply {
-            page += 1
-        }
-    }
+    override fun source(id: Long) = flowOf(30) { p -> p.page { i -> client.getNovelComment(id, i) } }
+    override fun reply(id: Long) = flowOf(30) { p -> p.page { i -> client.getNovelCommentReply(id, i) } }
 
     override suspend fun sendComment(parentId: Long?, id: Long, text: String) {
         client.sendNovelComment {
