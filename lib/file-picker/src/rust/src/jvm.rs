e7680a7dcb90d628cab8519305890af99edc1693
Dude so hot <djohn@fbi.gov>
2025-07-21 14:23:36 +0800

    Refine

--- lib/file-picker/src/rust/src/jvm.rs
+++ lib/file-picker/src/rust/src/jvm.rs
@@ -43,52 +43,24 @@ pub fn awaitFileSaver(env: JNIEnv, _class: JClass, ptr: *mut FFIClosure) -> jstr
 #[allow(non_snake_case)]
 #[jni_fn("top.kagg886.filepicker.internal.NativeFilePicker")]
 pub fn openFilePicker(mut env: JNIEnv, _class: JClass, ext: JObjectArray, title: JString, directory: JString) -> *mut FFIClosure {
-    // ext: Array<String>? => Option<Vec<String>>
-    let extensions = {
-        let raw = ext.as_raw(); // jobjectArray
-        if raw.is_null() {
-            None
-        } else {
-            let mut vec = Vec::new();
-            let len = env.get_array_length(&ext).unwrap_or(0);
-            for i in 0..len {
-                let jstr = env.get_object_array_element(&ext, i).unwrap();
-                let rust_str: String = env.get_string(&JString::from(jstr)).unwrap().into();
-                vec.push(rust_str);
-            }
-            Some(vec)
+    let dir = if directory.is_null() { String::from("~") } else { env.get_string(&directory).unwrap().into() };
+    let mut dialog = AsyncFileDialog::new().set_directory(dir);
+    if !ext.is_null() {
+        let mut vec = Vec::new();
+        let len = env.get_array_length(&ext).unwrap_or(0);
+        for i in 0..len {
+            let jstr = env.get_object_array_element(&ext, i).unwrap();
+            let rust_str: String = env.get_string(&JString::from(jstr)).unwrap().into();
+            vec.push(rust_str);
         }
-    };
-
-    // title: String? => Option<String>
-    let title: Option<String> = {
-        let raw = title.as_raw();
-        if raw.is_null() { None } else { Some(env.get_string(&title).unwrap().into()) }
-    };
-
-    // directory: String? => Option<String>
-    let directory: Option<String> = {
-        let raw = directory.as_raw();
-        if raw.is_null() { Some(String::from("~")) } else { Some(env.get_string(&directory).unwrap().into()) }
-    };
-
-    // === 弹出对话框 ===
-    let mut dialog = AsyncFileDialog::new();
-
-    if let Some(exts) = &extensions {
-        if !exts.is_empty() {
-            dialog = dialog.add_filter("filter", exts);
+        if !vec.is_empty() {
+            dialog = dialog.add_filter("filter", &vec);
         }
     }
-
-    if let Some(t) = &title {
-        dialog = dialog.set_title(t);
+    if !title.is_null() {
+        let title: String = env.get_string(&title).unwrap().into();
+        dialog = dialog.set_title(title)
     }
-
-    if let Some(dir) = &directory {
-        dialog = dialog.set_directory(dir);
-    }
-
     let fut = dialog.pick_file();
     let f = Box::new(|| futures::executor::block_on(fut));
     let bo = Box::new(FFIClosure { f });
