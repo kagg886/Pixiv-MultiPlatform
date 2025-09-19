41f5304112810fde92f658860e5130c809a2dca0
kagg886 <iveour@163.com>
2025-09-19 13:48:00 +0800

    插画详情页面 大对象塞进storage里保证不发生RPC error

--- composeApp/src/androidMain/kotlin/top/kagg886/pmf/util/serializable-wrapper.android.kt
+++ composeApp/src/androidMain/kotlin/top/kagg886/pmf/util/serializable-wrapper.android.kt
@@ -1,15 +1,19 @@
 package top.kagg886.pmf.util
 
+import com.russhwolf.settings.get
+import com.russhwolf.settings.set
 import java.io.Externalizable
 import java.io.ObjectInput
 import java.io.ObjectOutput
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
+import top.kagg886.pmf.backend.SystemConfig
 
-actual class SerializableWrapper<T : Any> : Externalizable {
+actual class PlatformSerializableWrapper<T : Any> : Externalizable, SerializableWrapper<T> {
 
     private var value: T? = null
 
@@ -36,10 +40,51 @@ actual class SerializableWrapper<T : Any> : Externalizable {
 
     override fun toString(): String = value.toString()
 
-    actual operator fun getValue(thisRef: Any?, property: KProperty<*>): T = value!!
+    actual override operator fun getValue(thisRef: Any?, property: KProperty<*>): T = value!!
 
     actual companion object {
-        actual fun <T : Any> makeItSerializable(value: T, clazz: KClass<T>): SerializableWrapper<T> = SerializableWrapper<T>().apply {
+        actual fun <T : Any> makeItSerializable(value: T, clazz: KClass<T>): SerializableWrapper<T> = PlatformSerializableWrapper<T>().apply {
+            this.value = value
+            this.clazz = clazz
+        }
+    }
+}
+
+actual class StorageSerializableWrapper<T : Any> actual constructor() : Externalizable, SerializableWrapper<T> {
+    private var key: String? = null
+
+    private var value: T? = null
+
+    private var clazz: KClass<T>? = null
+
+    @OptIn(InternalSerializationApi::class)
+    override fun writeExternal(out: ObjectOutput) {
+        out.writeUTF(key)
+        out.writeUTF(clazz!!.qualifiedName!!)
+        cache[key!!] = Json.encodeToString(clazz!!.serializer(), value!!).apply {
+            logger.d("write $key: ${clazz!!.qualifiedName}($length)")
+        }
+    }
+
+    @OptIn(InternalSerializationApi::class)
+    @Suppress("UNCHECKED_CAST")
+    override fun readExternal(input: ObjectInput) {
+        key = input.readUTF()
+        clazz = Class.forName(input.readUTF()).kotlin as KClass<T>
+
+        value = Json.decodeFromString(clazz!!.serializer(), cache[key!!]!!)
+
+        logger.d("restore $key: ${clazz!!.qualifiedName}")
+    }
+
+    override fun toString(): String = value.toString()
+
+    actual override operator fun getValue(thisRef: Any?, property: KProperty<*>): T = value!!
+
+    actual companion object {
+        internal val cache = SystemConfig.getConfig("cache")
+        actual fun <T : Any> makeItSerializable(key: String, value: T, clazz: KClass<T>): SerializableWrapper<T> = StorageSerializableWrapper<T>().apply {
+            this.key = key
             this.value = value
             this.clazz = clazz
         }
--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/util/serializable-wrapper.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/util/serializable-wrapper.kt
@@ -3,13 +3,30 @@ package top.kagg886.pmf.util
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-inline fun <reified T : Any> wrap(value: T): SerializableWrapper<T> = SerializableWrapper.makeItSerializable(value, T::class)
+interface SerializableWrapper<T> {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T
+}
 
-expect class SerializableWrapper<T : Any> {
+@Deprecated("use platform() instead", ReplaceWith("platform()", imports = ["top.kagg886.pmf.util.platform"]))
+inline fun <reified T : Any> wrap(value: T): SerializableWrapper<T> = PlatformSerializableWrapper.makeItSerializable(value, T::class)
+
+inline fun <reified T : Any> platform(value: T): SerializableWrapper<T> = PlatformSerializableWrapper.makeItSerializable(value, T::class)
+
+inline fun <reified T : Any> storage(key: String, value: T): SerializableWrapper<T> = StorageSerializableWrapper.makeItSerializable(key, value, T::class)
+
+expect class PlatformSerializableWrapper<T : Any> {
 
     companion object {
         fun <T : Any> makeItSerializable(value: T, clazz: KClass<T>): SerializableWrapper<T>
     }
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T
+}
+
+expect class StorageSerializableWrapper<T : Any>() : SerializableWrapper<T> {
+    companion object {
+        fun <T : Any> makeItSerializable(key: String, value: T, clazz: KClass<T>): SerializableWrapper<T>
+    }
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): T
 }
